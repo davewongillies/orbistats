16f8ee4117644c937fd95194938da1aa64c704da
David Gillies <dave.gillies@gmail.com>
2018-11-23 08:24:18 -0800

    Add connection_status()

--- orbi/orbi.py
+++ orbi/orbi.py
@@ -2,15 +2,25 @@ import base64
 import urllib2
 import re
 
+
 def statistics(username, password):
-    request = urllib2.Request('http://orbilogin.com/RST_statistic.htm')
-    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-    request.add_header("Authorization", "Basic %s" % base64string)
+    return get('RST_statistic', username, password)
+
+
+def connection_status(username, password):
+    return get('RST_conn_status', username, password)
+
+
+def get(url, username, password):
+    request = urllib2.Request('http://orbilogin.com/{}.htm'.format(url))
+    base64string = base64.encodestring('{}:{}'.format(username, password)).strip('\n')
+    request.add_header("Authorization", "Basic {}".format(base64string))
 
     response = urllib2.urlopen(request)
     html = response.read()
-
-    js_var_pattern = re.compile(r'^var (?P<name>[A-Za-z_0-9]+?)\s*?="?(?P<value>.*?)"?;$', flags=re.MULTILINE | re.UNICODE)
+    js_var_pattern = re.compile(
+        r'^var (?P<name>[A-Za-z_0-9]+?)\s*?="?(?P<value>.*?)"?;$',
+        flags=re.MULTILINE | re.UNICODE)
     matches = re.findall(js_var_pattern, html)
 
     response.close()
